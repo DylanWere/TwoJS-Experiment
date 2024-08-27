52f62ab79e3d64458afacd7a0580d060d3feee68
DylanWere <83937218+DylanWere@users.noreply.github.com>
2024-08-26 17:31:16 -0700

    Format documents with prettier

--- animate.js
+++ animate.js
@@ -1,7 +1,7 @@
 const two = new Two({
-    type: Two.Types.canvas,
-    fullscreen: true,
-    autostart: true
+  type: Two.Types.canvas,
+  fullscreen: true,
+  autostart: true,
 }).appendTo(document.body);
 
 /* create road */
@@ -10,19 +10,17 @@ var amount = 32;
 var points = [];
 
 for (let i = 0; i < amount; i++) {
+  var pct = i / (amount - 1);
+  var theta = Math.PI * 2 * pct;
+  var x = (two.width / 20) * Math.cos(theta);
+  var y = pct * two.height * 1.5;
 
-    var pct = i / (amount - 1);
-    var theta = Math.PI * 2 * pct;
-    var x = two.width / 20 * Math.cos(theta);
-    var y = pct * two.height * 1.5;
-
-    points.push(new Two.Anchor(x, y));
-    
+  points.push(new Two.Anchor(x, y));
 }
 
 var sidewalk = two.makeCurve(points, true);
 sidewalk.translation.set(two.width / 2, two.height / 2);
-sidewalk.noFill().stroke = '#ccc';
+sidewalk.noFill().stroke = "#ccc";
 sidewalk.linewidth = two.width / 4;
 
 var road = two.makeCurve(points, true);
@@ -35,82 +33,82 @@ road.linewidth = two.width / 6;
 var dashes = [];
 
 for (let j = 0; j < 10; j++) {
-    
-    var gutter = -10;
-    var length = two.height / (amount + gutter);
-    var dash = two.makeLine((0, -length, 0, length));
+  var gutter = -10;
+  var length = two.height / (amount + gutter);
+  var dash = two.makeLine((0, -length, 0, length));
 
-    dash.noFill().stroke = '#fff';
-    dash.linewidth = 3;
-    dash.pct = j / (10 - 1);
+  dash.noFill().stroke = "#fff";
+  dash.linewidth = 3;
+  dash.pct = j / (10 - 1);
 
-    dash.translation.copy(points[j]).addSelf(road.translation);
-    dashes.push(dash);
-
-    
+  dash.translation.copy(points[j]).addSelf(road.translation);
+  dashes.push(dash);
 }
 
 //cached variable that go in the nimation loop
 
-var a = new Two.Vector(), b = new Two.Vector();
+var a = new Two.Vector(),
+  b = new Two.Vector();
 var velocity = 0.0125;
 
-two.bind('update', function(frameCount, timeDelta){
-    if (!timeDelta) {
-        return;
+two.bind("update", function (frameCount, timeDelta) {
+  if (!timeDelta) {
+    return;
+  }
+
+  for (var i = 0; i < amount; i++) {
+    //animate the road
+    var v = points[i];
+    var pct = i / (amount - 1);
+    var offset = pct * Math.PI * 2;
+    var theta = offset + frameCount / 20;
+    v.x = (two.width / 20) * Math.cos(theta);
+
+    var length = dashes.length;
+
+    if (i > length - 1) {
+      continue;
     }
 
-    for (var i = 0; i < amount; i++) {
+    //animate the dashes
 
-        //animate the road
-        var v = points[i];
-        var pct = i / (amount - 1);
-        var offset = pct * Math.PI * 2;
-        var theta = offset + frameCount / 20;
-        v.x = two.width / 20 * Math.cos(theta);
+    var dash = dashes[i];
+    //assign calculation of the vector on the road to 'a'
+    road.getPointAt(dash.pct, a);
+    //get an arbitrary vector right behind 'a' in order to get the angle for the rotation of the dash
+    road.getPointAt(dash.pct - 0.01, b);
+    dash.translation.copy(a).addSelf(road.translation);
+    dash.rotation = Two.Vector.angleBetween(a, b);
 
-        var length = dashes.length;
-
-        if (i > length - 1) {
-            continue;
-        }
-
-        //animate the dashes
-
-        var dash = dashes[i];
-        //assign calculation of the vector on the road to 'a'
-        road.getPointAt(dash.pct, a);
-        //get an arbitrary vector right behind 'a' in order to get the angle for the rotation of the dash
-        road.getPointAt(dash.pct - 0.01, b);
-        dash.translation.copy(a).addSelf(road.translation);
-        dash.rotation = Two.Vector.angleBetween(a, b);
-
-        dash.pct = mod(dash.pct + velocity, 1);
-    }
+    dash.pct = mod(dash.pct + velocity, 1);
+  }
 });
 
 //add mouse interaction
-window.addEventListener('mousemove', function(e) {
+window.addEventListener(
+  "mousemove",
+  function (e) {
     var pct = (e.clientY / window.innerHeight - 0.5) * 2;
     velocity = pct * 0.03;
-}, false);
+  },
+  false
+);
 
-window.addEventListener('touchmove', function(e) {
-    var touch = e.changedTouches[1];
+window.addEventListener("touchmove", function (e) {
+  var touch = e.changedTouches[1];
 
-    if (!touch){
-        return;
-    }
+  if (!touch) {
+    return;
+  }
 
-    var pct = (e.clientY / window.innerHeight - 0.5) * 2;
-    velocity = pct * 0.3; 
+  var pct = (e.clientY / window.innerHeight - 0.5) * 2;
+  velocity = pct * 0.3;
 });
 
 function mod(v, l) {
-    while (v < 0) {
-        v += l;
-    }
+  while (v < 0) {
+    v += l;
+  }
 
-    return v%l;
+  return v % l;
 }
-
--- animateMouse.js
+++ animateMouse.js
@@ -1,7 +1,7 @@
 const two = new Two({
-    type: Two.Types.canvas,
-    fullscreen: true,
-    autostart: true
+  type: Two.Types.canvas,
+  fullscreen: true,
+  autostart: true,
 }).appendTo(document.body);
 
 /* create road */
@@ -11,18 +11,16 @@ var points = [];
 var mouseX = 0;
 
 for (let i = 0; i < amount; i++) {
+  var pct = i / (amount - 1);
+  var x = mouseX;
+  var y = pct * two.height * 1.5;
 
-    var pct = i / (amount - 1);
-    var x = mouseX;
-    var y = pct * two.height * 1.5;
-
-    points.push(new Two.Anchor(x, y));
-    
+  points.push(new Two.Anchor(x, y));
 }
 
 var sidewalk = two.makeCurve(points, true);
 sidewalk.translation.set(two.width / 2, two.height / 2);
-sidewalk.noFill().stroke = '#ccc';
+sidewalk.noFill().stroke = "#ccc";
 sidewalk.linewidth = two.width / 4;
 
 var road = two.makeCurve(points, true);
@@ -35,85 +33,78 @@ road.linewidth = two.width / 6;
 var dashes = [];
 
 for (let j = 0; j < 10; j++) {
-    
-    var gutter = -10;
-    var length = two.height / (amount + gutter);
-    var dash = two.makeLine((0, -length, 0, length));
+  var gutter = -10;
+  var length = two.height / (amount + gutter);
+  var dash = two.makeLine((0, -length, 0, length));
 
-    dash.noFill().stroke = '#fff';
-    dash.linewidth = 3;
-    dash.pct = j / (10 - 1);
+  dash.noFill().stroke = "#fff";
+  dash.linewidth = 3;
+  dash.pct = j / (10 - 1);
 
-    dash.translation.copy(points[j]).addSelf(road.translation);
-    dashes.push(dash);
-
-    
+  dash.translation.copy(points[j]).addSelf(road.translation);
+  dashes.push(dash);
 }
 
 //cached variable that go in the nimation loop
 
-var a = new Two.Vector(), b = new Two.Vector();
+var a = new Two.Vector(),
+  b = new Two.Vector();
 var velocity = 0.0125;
 
-two.bind('update', function(frameCount, timeDelta){
-    if (!timeDelta) {
-        return;
+two.bind("update", function (frameCount, timeDelta) {
+  if (!timeDelta) {
+    return;
+  }
+
+  for (var i = 0; i < amount; i++) {
+    //animate the road
+
+    var topVal = amount - i;
+    var v = points[topVal - 1];
+    var prev = points[topVal - 2];
+
+    if (topVal - 1 == 0) {
+      //smoothing - get the new mouse x and average it with the last
+      v.x =
+        (mouseX - two.width / 2 + points[topVal].x + points[topVal + 1].x) / 3;
+    } else if (i < amount - 1) {
+      v.x = prev.x;
     }
 
-    
+    var length = dashes.length;
 
-    for (var i = 0; i < amount; i++) {
-
-        //animate the road
-
-        var topVal = amount-i; 
-        var v = points[topVal - 1];
-        var prev = points[topVal - 2];
-        
-        if (topVal - 1 == 0) {
-            //smoothing - get the new mouse x and average it with the last
-            v.x = ((mouseX - two.width / 2) + points[topVal].x + points[topVal + 1].x)/3;
-        } else if (i < amount-1) {
-            v.x = prev.x;
-        }
-
-       
-        
-
-        var length = dashes.length;
-
-        if (i > length - 1) {
-            continue;
-        }
-
-        //animate the dashes
-        //this can remain the same
-        var dash = dashes[i];
-        //assign calculation of the vector on the road to 'a'
-        road.getPointAt(dash.pct, a);
-        //get an arbitrary vector right behind 'a' in order to get the angle for the rotation of the dash
-        road.getPointAt(dash.pct - 0.01, b);
-        dash.translation.copy(a).addSelf(road.translation);
-        dash.rotation = Two.Vector.angleBetween(a, b);
-
-        dash.pct = mod(dash.pct + velocity, 1);
+    if (i > length - 1) {
+      continue;
     }
+
+    //animate the dashes
+    //this can remain the same
+    var dash = dashes[i];
+    //assign calculation of the vector on the road to 'a'
+    road.getPointAt(dash.pct, a);
+    //get an arbitrary vector right behind 'a' in order to get the angle for the rotation of the dash
+    road.getPointAt(dash.pct - 0.01, b);
+    dash.translation.copy(a).addSelf(road.translation);
+    dash.rotation = Two.Vector.angleBetween(a, b);
+
+    dash.pct = mod(dash.pct + velocity, 1);
+  }
 });
 
 //add mouse interaction
 var mouseX = 0;
-window.addEventListener('mousemove', function(e) {
-
+window.addEventListener(
+  "mousemove",
+  function (e) {
     mouseX = e.clientX;
-
-}, false);
-
+  },
+  false
+);
 
 function mod(v, l) {
-    while (v < 0) {
-        v += l;
-    }
+  while (v < 0) {
+    v += l;
+  }
 
-    return v%l;
+  return v % l;
 }
-
